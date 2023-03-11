7d9ab2210d4513bda10d13241afbb5decf4f3d61
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2023-03-11 19:24:41 +0900

    Time: 280 ms (5.03%), Space: 14.4 MB (7.67%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.swift
@@ -0,0 +1,39 @@
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        let board = (0...65536).map { $0 * $0 }
+        let results = Array(0...65536)
+        
+        var front = 0
+        var back = board.count - 1
+        
+        while front <= back {
+            let mid = (front + back) / 2
+            
+            if board[mid] == x {
+                return results[mid]
+            } else if board[mid] > x {
+                if mid == 0 {
+                    return results[0]
+                } else {
+                    if board[mid-1] < x {
+                        return results[mid-1]
+                    } else {
+                        back = mid - 1
+                    }
+                }
+            } else {
+                if mid == board.count - 1 {
+                    return results[board.count - 1]
+                } else {
+                    if board[mid+1] > x {
+                        return results[mid]
+                    } else {
+                        front = mid + 1
+                    }
+                }
+            }
+        }
+        
+        return 0
+    }
+}
