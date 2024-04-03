61c7ddc08603134415210df20a5f1bcd73c57d79
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-04-03 12:47:34 +0900

    Time: 27 ms (68.55%), Space: 15.6 MB (62.26%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.swift
+++ 0695-max-area-of-island/0695-max-area-of-island.swift
@@ -1,31 +1,33 @@
 class Solution {
     func maxAreaOfIsland(_ grid: [[Int]]) -> Int {
         var grid = grid
-        var areas = [0]
+        var result = 0
         
         for i in 0..<grid.count {
             for j in 0..<grid[i].count {
                 var area = 0
-                
                 traverse(&grid, i, j, &area)
-                areas.append(area)
+                
+                if area > 0 {
+                    result = max(result, area)
+                }
             }
         }
         
-        return areas.max()!
+        return result
     }
     
-    func traverse(_ grid: inout [[Int]], _ r: Int, _ c: Int, _ area: inout Int) {
-        guard (0..<grid.count) ~= r && (0..<grid[r].count) ~= c && grid[r][c] == 1 else {
+    func traverse(_ grid: inout [[Int]], _ row: Int, _ col: Int, _ area: inout Int) {
+        guard (0..<grid.count) ~= row && (0..<grid[row].count) ~= col && grid[row][col] == 1 else {
             return
         }
         
-        grid[r][c] = 0
         area += 1
+        grid[row][col] = 0
         
-        traverse(&grid, r+1, c, &area)
-        traverse(&grid, r-1, c, &area)
-        traverse(&grid, r, c+1, &area)
-        traverse(&grid, r, c-1, &area)
+        traverse(&grid, row-1, col, &area)
+        traverse(&grid, row+1, col, &area)
+        traverse(&grid, row, col-1, &area)
+        traverse(&grid, row, col+1, &area)
     }
 }
