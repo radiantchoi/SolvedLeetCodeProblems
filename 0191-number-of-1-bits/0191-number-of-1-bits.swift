e1753aa6fe46d18b4ff6c90158b1edee2b701cd9
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2024-04-24 13:41:46 +0900

    Time: 7 ms (16.67%), Space: 15.5 MB (6.75%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.swift
+++ 0191-number-of-1-bits/0191-number-of-1-bits.swift
@@ -1,17 +1,6 @@
 class Solution {
     func hammingWeight(_ n: Int) -> Int {
-        var n = n
-        var result = 0
-        
-        while n > 0 {
-            let (div, mod) = n.quotientAndRemainder(dividingBy: 2)
-            if mod == 1 {
-                result += 1
-            }
-            
-            n = div
-        }
-        
-        return result
+        let digits = String(n, radix: 2)
+        return digits.filter { $0 == "1" }.count
     }
 }
