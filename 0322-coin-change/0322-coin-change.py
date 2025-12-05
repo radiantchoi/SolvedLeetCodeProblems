430e64554fe2d2eda65a72fcd666b3aa00bdbbf9
Gordon Choi <80261919+radiantchoi@users.noreply.github.com>
2025-12-05 18:33:34 +0900

    Time: 303 ms (98.61%), Space: 18.5 MB (36.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,20 +4,20 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [-1 for _ in range(amount + 1)]
         
+        dp[0] = 0
         q = deque([[0, 0]])
 
         while q:
             current = q.popleft()
 
-            if current[1] > amount:
-                continue
-            if dp[current[1]] > 0:
-                continue
-
-            dp[current[1]] = current[0]
-
             for coin in coins:
-                new = [current[0] + 1, current[1] + coin]
-                q.append(new)
+                estimated = current[1] + coin
+
+                if estimated <= amount and dp[estimated] == -1:
+                    used = current[0] + 1
+                    dp[estimated] = used
+
+                    new = [used, estimated]
+                    q.append(new)
 
         return dp[amount]
